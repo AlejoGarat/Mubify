bb4bb7d3acba7705757a441a1656c761871d4b9f
AlejoGarat <alejogaratd@gmail.com>
2023-12-23 16:16:27 -0300

    updated migrations

--- scripts/new_movies.py
+++ scripts/new_movies.py
@@ -8,9 +8,10 @@ sql_file = '20231223153720_new_movies.up.sql'
 with open(sql_file, 'w') as f:
     for index, row in df.iterrows():
         movie_id = row['movieId']
-        title = row['title']
+        title = row['title'].replace("'", "''")  # Escapar comillas simples
 
         sql_query = f"INSERT INTO movies (movie_id, title) VALUES ({movie_id}, '{title}');\n"
         f.write(sql_query)
 
 print(f"Se generaron las consultas SQL en {sql_file}")
+
