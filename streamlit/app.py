f508c8f1260a3a61b853d4df49b2ef8eb77d09a1
AlejoGarat <alejogaratd@gmail.com>
2024-02-18 11:52:57 -0300

    showing similar movies

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,13 +1,45 @@
 import streamlit as st
 import requests
+import pandas as pd
+import pickle
+from sklearn.neighbors import NearestNeighbors
 
 api_url = "http://127.0.0.1:5000/api/"
+current_page = 1
 
 def main():
     st.title('Mubify')
     
-    movies_info = get_movie_info(9, 1)
+    if "current_page" not in st.session_state:
+        st.session_state.current_page = 1
+    
+    if not st.session_state.get("logged_in"):
+        login()
+    else:
+        show_app_content()
+        
+    
+def login():
+    st.subheader("Login")
+    user_id = st.text_input("User ID")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"):
+        if validate_login(user_id, password):
+            st.session_state.logged_in = True 
+            st.session_state.user_id = user_id 
+            st.experimental_rerun()
+        else:
+            st.error("Invalid user ID or password!")
+            
+def show_app_content():
+    user_id_container = st.empty()
+    user_id_container.text(f"User ID: {st.session_state.user_id}")
+    movies_info = get_movie_info(9, current_page)
     display_movies(movies_info)
+    show_pagination(current_page)
+
+def validate_login(user_id, password):
+    return True
     
 def display_movies(movies_info):
     for i in range(0, len(movies_info), 3):
@@ -38,11 +70,25 @@ def show_similar_movies(movie_title):
             st.image(similar_movie["movie_url"], caption=similar_movie["movie_title"])
 
 def get_similar_movies(movie_title):
-    return [
-        {"movie_title": "Similar Movie 1", "movie_url": "https://example.com/similar_image1.jpg"},
-        {"movie_title": "Similar Movie 2", "movie_url": "https://example.com/similar_image2.jpg"},
-        {"movie_title": "Similar Movie 3", "movie_url": "https://example.com/similar_image3.jpg"},
-    ]
+    similar_movies = []
+    movies_ds = pd.read_csv("similar_movies_ds.csv")
+    movie = movies_ds[movies_ds['title'] == movie_title].drop(['title'], axis=1)
+    
+    with open('notebooks/knn_model.pkl', 'rb') as f:
+        loaded_knn_model = pickle.load(f)
+    
+    distances, indices = loaded_knn_model.kneighbors(movie)
+
+    for i in range(len(distances.flatten())):
+        if i != 0:
+            movie_info = {
+                "movie_title": movies_ds['title'].iloc[indices.flatten()[i]],
+                "movie_url": "https://m.media-amazon.com/images/I/71HQiOZsZ6L._AC_UF894,1000_QL80_.jpg"
+            }
+            similar_movies.append(movie_info)
+    
+    return similar_movies
 
 if __name__ == "__main__":
     main()
+
